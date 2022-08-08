37c6649f99855e9420e400027f78cf5032b32fe9
tdrk18 <s18.todoroki@gmail.com>
2022-08-08 15:57:18 +0900

    Update script.sh

--- bin/script.sh
+++ bin/script.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+script_dir="$( cd "$( dirname "${BASH_SOURCE}" )" && pwd )"
 config_file="$script_dir/.request_build_config"
 
-one=$1
-two=$2
-three=${3:-main}
+build_flavor=$1
+build_type=$2
+branch=${3:-main}
 
-echo "one:   $one"
-echo "two:   $two"
-echo "three: $three"
+echo "build_flavor: $build_flavor"
+echo "build_type:   $build_type"
+echo "branch:       $branch"
+
+curl \
+    --header "Content-Type: application/json" \
+    --data '{"build_parameters": {"BUILD_FLAVOR": "'$build_flavor'", "BUILD_TYPE": "'$build_type'"}}' \
+    --request POST \
+    https://circleci.com/api/v1.1/project/github/tdrk18/BitBox/tree/$revision?circle-token=$CIRCLE_TOKEN
